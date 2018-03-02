041a9ecb08ec3e779d745d82c48249ae4df00283
Supersmf <spec16@InzhAuto.local>
2018-03-02 16:09:19 +0200

    advanced-zeros: part of tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let count = 0;
+    let num = number;
+    let currBase = base;
+    let divider = null;
+    let divide = () => 1 <= (num / divider);
+
+    for (let i = 2; i <= currBase; i++) {
+        while (!(currBase % i)) {
+            currBase /= i;
+            divider = i;
+        }
+    }
+
+    while (divide()) {
+        num = Math.floor(num / divider);
+        count += num;
+    }
+    // if(base > 100) count /= 2;
+    // if(base > 200) count /= 3;
+    return Math.floor(count);
 }
